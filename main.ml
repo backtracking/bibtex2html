2c64426752da22508d568ae12c26ab58722ca2bf
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2002-06-24 07:43:29 +0000

    toutes les options acceptent "-" a la place de "--"
    git-svn-id: https://www.lri.fr/svn/demons/bibtex2html/trunk@255 6aee9057-109c-4a6d-8c93-4c3f535006fd

--- main.ml
+++ main.ml
@@ -14,7 +14,7 @@
  * (enclosed in the file GPL).
  *)
 
-(*i $Id: main.ml,v 1.48 2002-06-24 07:35:58 filliatr Exp $ i*)
+(*i $Id: main.ml,v 1.49 2002-06-24 07:43:29 filliatr Exp $ i*)
 
 (*s Main module of bibtex2html. *)
 
@@ -345,51 +345,51 @@ let parse () =
   let rec parse_rec = function
 
     (* General aspect of the web page *)
-    | ("-t" | "--title") :: s :: rem ->
+    | ("-t" | "-title" | "--title") :: s :: rem ->
 	title := s; title_spec := true; parse_rec rem
-    | ("-t" | "--title") :: [] ->
+    | ("-t" | "-title" | "--title") :: [] ->
 	usage()
-    | ("-bg" | "--background") :: s :: rem ->
+    | ("-bg" | "-background" | "--background") :: s :: rem ->
 	Html.bgcolor := Some s; parse_rec rem
-    | ("-bg" | "--background") :: [] ->
+    | ("-bg" | "-background" | "--background") :: [] ->
 	usage()
-    | ("-css" | "--style-sheet") :: f :: rem ->
+    | ("-css" | "-style-sheet" | "--style-sheet") :: f :: rem ->
 	Html.css := Some f; parse_rec rem
-    | ("-css" | "--style-sheet") :: [] ->
+    | ("-css" | "-style-sheet" | "--style-sheet") :: [] ->
 	usage()
     | ("-footer" | "--footer") :: s :: rem ->
 	user_footer := s; parse_rec rem
     | ("-footer" | "--footer") :: [] ->
 	usage()
-    | ("-s" | "--style") :: s :: rem ->
+    | ("-s" | "-style" | "--style") :: s :: rem ->
 	style := s; parse_rec rem
-    | ("-s" | "--style") :: [] ->
+    | ("-s" | "-style" | "--style") :: [] ->
 	usage()
-    | ("-noabstract" | "--no-abstract") :: rem ->
+    | ("-noabstract" | "-no-abstract" | "--no-abstract") :: rem ->
 	print_abstract := false; parse_rec rem
-    | ("-nokeywords" | "--no-keywords") :: rem ->
+    | ("-nokeywords" | "-no-keywords" | "--no-keywords") :: rem ->
 	print_keywords := false; parse_rec rem
-    | ("-nokeys" | "--no-keys") :: rem -> 
+    | ("-nokeys" | "-no-keys" | "--no-keys") :: rem -> 
 	nokeys := true; parse_rec rem
-    | ("-rawurl" | "--raw-url") :: rem -> 
+    | ("-rawurl" | "-raw-url" | "--raw-url") :: rem -> 
 	raw_url := true; parse_rec rem
 (*i
     | ("-tu" | "-titleurl" | "--title-url") :: rem -> 
 	title_url := true; parse_rec rem
 i*)
-    | ("-heveaurl" | "--hevea-url") :: rem -> 
+    | ("-heveaurl" | "-hevea-url" | "--hevea-url") :: rem -> 
 	Latexscan.hevea_url := true; parse_rec rem
-    | ("-noheader" | "--no-header") :: rem ->
+    | ("-noheader" | "-no-header" | "--no-header") :: rem ->
 	print_header := false; parse_rec rem
-    | ("-nofooter" | "--no-footer") :: rem ->
+    | ("-nofooter" | "-no-footer" | "--no-footer") :: rem ->
 	print_footer := false; parse_rec rem
-    | ("-f" | "--field") :: s :: rem ->
+    | ("-f" | "-field" | "--field") :: s :: rem ->
 	add_field s; parse_rec rem
-    | ("-f" | "--field") :: [] ->
+    | ("-f" | "-field" | "--field") :: [] ->
 	usage()
-    | ("-nf" | "--named-field") :: s :: name :: rem ->
+    | ("-nf" | "-named-field" | "--named-field") :: s :: name :: rem ->
 	add_named_field s name; parse_rec rem
-    | ("-nf" | "--named-field") :: ([_] | []) ->
+    | ("-nf" | "-named-field" | "--named-field") :: ([_] | []) ->
 	usage()
     | ("-ln" | "-labelname" | "--labelname" | "--label-name") :: rem ->
 	use_label_name := true; parse_rec rem
@@ -401,19 +401,19 @@ i*)
 	table := false; parse_rec rem
  
     (* Controlling the translation *)
-    | ("-m" | "--macros-from") :: f :: rem ->
+    | ("-m" | "-macros-from" | "--macros-from") :: f :: rem ->
 	read_macros f; parse_rec rem
-    | ("-m" | "--macros-from") :: [] ->
+    | ("-m" | "-macros-from" | "--macros-from") :: [] ->
 	usage()
  
     (* Sorting the entries *)
-    | ("-d" | "--sort-by-date") :: rem ->
+    | ("-d" | "-sort-by-date" | "--sort-by-date") :: rem ->
 	sort := By_date; parse_rec rem
-    | ("-a" | "--sort-as-bibtex") :: rem ->
+    | ("-a" | "-sort-as-bibtex" | "--sort-as-bibtex") :: rem ->
 	sort := By_author; parse_rec rem
-    | ("-u" | "--unsorted") :: rem ->
+    | ("-u" | "-unsorted" | "--unsorted") :: rem ->
 	sort := Unsorted; parse_rec rem
-    | ("-r" | "--reverse-sort") :: rem ->
+    | ("-r" | "-reverse-sort" | "--reverse-sort") :: rem ->
 	reverse_sort := true; parse_rec rem
 
     (* Options for selecting keys *)
@@ -423,39 +423,39 @@ i*)
 	parse_rec rem
     | ("-citefile" | "--citefile") :: [] ->
 	usage()
-    | ("-e" | "--exclude") :: k :: rem ->
+    | ("-e" | "-exclude" | "--exclude") :: k :: rem ->
 	add_exclude k; parse_rec rem
-    | ("-e" | "--exclude") :: [] ->
+    | ("-e" | "-exclude" | "--exclude") :: [] ->
 	usage()
  
     (* Miscellaneous options *)
-    | ("-o" | "--output") :: f :: rem ->
+    | ("-o" | "-output" | "--output") :: f :: rem ->
 	output_file := f;
 	parse_rec rem
-    | ("-o" | "--output") :: [] ->
+    | ("-o" | "-output" | "--output") :: [] ->
 	usage()
     | ("-nobibsource" | "--nobibsource") :: rem ->
 	bib_entries := false; parse_rec rem
-    | ("-nodoc" | "--nodoc" | "--no-doc") :: rem -> 
+    | ("-nodoc" | "--nodoc" | "-no-doc" | "--no-doc") :: rem -> 
 	nodoc := true; parse_rec rem
-    | ("-noexpand" | "--no-expand") :: rem -> 
+    | ("-noexpand" | "-no-expand" | "--no-expand") :: rem -> 
 	expand_abbrev_in_bib_output := false; parse_rec rem
-    | ("-i" | "--ignore-errors") :: rem ->
+    | ("-i" | "-ignore-errors" | "--ignore-errors") :: rem ->
 	ignore_bibtex_errors := true; parse_rec rem
 
     | ("-suffix" | "--suffix") :: s :: rem ->
 	file_suffix := s; link_suffix := s; parse_rec rem
-    | ("-fsuffix" | "--file-suffix") :: s :: rem ->
+    | ("-fsuffix" | "-file-suffix" | "--file-suffix") :: s :: rem ->
 	file_suffix := s; parse_rec rem
-    | ("-lsuffix" | "--link-suffix") :: s :: rem ->
+    | ("-lsuffix" | "-link-suffix" | "--link-suffix") :: s :: rem ->
 	link_suffix := s; parse_rec rem
-    | ("-suffix" | "--suffix" | "-fsuffix" | "--file-suffix" |
-       "-lsuffix" | "--link-suffix") :: [] ->
+    | ("-suffix" | "--suffix" | "-fsuffix" | "--file-suffix" | "-file-suffix" |
+       "-lsuffix" | "-link-suffix" | "--link-suffix") :: [] ->
 	usage()
 
-    | ("-c" | "--command") :: s :: rem ->
+    | ("-c" | "-command" | "--command") :: s :: rem ->
 	command := s; parse_rec rem
-    | ("-c" | "--command") :: [] ->
+    | ("-c" | "-command" | "--command") :: [] ->
 	usage()
     | ("-h" | "-help" | "-?" | "--help") :: rem ->
 	usage ()
@@ -464,7 +464,7 @@ i*)
     | ("-warranty" | "--warranty") :: _ ->
 	Copying.banner "bibtex2html"; Copying.copying(); exit 0
 
-    | ("-q" | "--quiet") :: rem ->
+    | ("-q" | "-quiet" | "--quiet") :: rem ->
 	Options.quiet := true; parse_rec rem
     | ("-debug" | "--debug") :: rem ->
 	Options.debug := true; parse_rec rem
